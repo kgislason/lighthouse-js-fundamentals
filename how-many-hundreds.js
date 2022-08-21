b3ea7925e2545c517ccda98018c9145930391ec1
Kristy Gislason <kristygislason@gmail.com>
2022-08-21 16:27:03 -0700

    Add how many hindreds functions to determine how many 100s fit into a number. Area caltulator functiosn for circle triangle and square

---
+++ how-many-hundreds.js
@@ -0,0 +1,24 @@
+// As humanity gets closer to living on Mars, we must start sending supplies to the red planet now, to prepare for our arrival. So Canada is going to start to send containers of maple syrup. Each container on the space ship can hold exactly 100 bottles of maple syrup. A container will only be sent if it's full.
+
+// Given a certain number of bottles, we need to determine how many containers can be filled and sent. In this challenge, we will write a function that determines how many hundreds can be made from a number.
+
+//When this function is given a number, it should return how many hundreds fit into that number.
+
+const howManyHundreds = function(number) {
+  let result = number / 100;
+
+  // round down to the nearest whole number;
+  result = Math.floor(result);
+
+  return result;
+}
+
+// Call the function
+console.log(howManyHundreds(2005));
+
+// Test cases
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
