40ecc880c554ee94c1956e7289cc47674e4c4a0a
Kristy Gislason <kristygislason@gmail.com>
2022-08-20 23:14:31 -0700

    Create function to determine final position of photographer at parade x and y coordinates

---
+++ x-marks-spot.js
@@ -0,0 +1,24 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north'];
+
+const finalPosition = function (moves) {
+
+  let x = 0;
+  let y = 0;
+
+  for (let move of moves ) {
+    if (move === "north") {
+      y++;
+    } else if (move === "south") {
+      y--;
+    } else if (move === "east") {
+      x++;
+    } else if (move === "west") {
+      x--;
+    }
+  }
+
+  return [x, y];
+
+}
+
+console.log(finalPosition(moves));
