04f08732b277b6f592ccf8245429cb42365805e1
Kristy Gislason <kristygislason@gmail.com>
2022-08-31 10:36:54 -0700

    Start loopy lighthouse stretch activity

---
+++ loopy-lighthouse-2.js
@@ -0,0 +1,13 @@
+/**
+ * Challenge
+ * Wrap the code you've written in a function called loopyLighthouse(range,
+ * multiples, words) that you call with 3 parameters.
+ */
+
+
+/**
+ * - range should be an array of 2 numbers representing the start and end values for the loop.
+ * - multiples should be an array of 2 numbers representing the multiples you want to replace with words.
+ * - words should be an array of 2 strings representing the words that will replace the multiples.
+ */
+
